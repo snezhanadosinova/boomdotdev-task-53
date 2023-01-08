a3bad3b9133324b076f1b153e8737edc59a52855
snezhanadosinova <sdosinova@gmail.com>
2023-01-08 19:11:28 +0200

    feat: added the data-price to .product element

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  var productElement = document.querySelector(".product");
 
+  var productPrice = document.querySelector(".price").innerHTML;
+
+  productElement.setAttribute("data-price", productPrice);
 });
